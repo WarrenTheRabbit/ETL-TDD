7adf4044592c1a812b5922fa29f5b5e932afd6dd
Warren <rabbitsinwarrens@gmail.com>
2023-06-02 07:01:21 +0000

    Update print message.

--- tests/utilities/presentation.py
+++ tests/utilities/presentation.py
@@ -7,7 +7,7 @@ colorama.init()
 
 def print_objects_in_bucket(message:str, mock_bucket:MockS3Bucket):
     print("*"*120)
-    print(f"{Fore.MAGENTA}The objects below {message} {Style.RESET_ALL}")
+    print(f"{Fore.MAGENTA}These objects {message} {Style.RESET_ALL}")
     print("*"*120)
 
     objects =  mock_bucket.list_all()
@@ -26,19 +26,3 @@ def print_objects_in_bucket(message:str, mock_bucket:MockS3Bucket):
             print(Fore.YELLOW + s3_path + Style.RESET_ALL)  
         elif 'optimised' in s3_path:
             print(Fore.BLUE + s3_path + Style.RESET_ALL)
-    
-
-    
-
-
-def with_s3_content_reports(test_function):
-    def wrapped_test(spark, mock_bucket):
-        print_objects_in_bucket("existed before testing the modules:", mock_bucket)
-        
-        try:
-            test_function(spark, mock_bucket)
-            print_objects_in_bucket("were created during the test:", mock_bucket)
-        finally:
-            print_objects_in_bucket("exist after the test:", mock_bucket)
-    
-    return wrapped_test
