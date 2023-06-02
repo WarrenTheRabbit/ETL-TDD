a4ddd9a0cffbb5c352367dc86022b6033cc6fe34
Warren <rabbitsinwarrens@gmail.com>
2023-06-02 06:06:55 +0000

    Rename Actions to TestActions.

--- tests/utilities/actions.py
+++ tests/utilities/actions.py
@@ -1,14 +1,14 @@
 from tests.utilities.presentation import print_objects_in_bucket
 
-class Actions:
+class TestActions:
     def __init__(self, pre_test, during_test, post_test):
         self.pre_test = pre_test
         self.during_test = during_test
         self.post_test = post_test
 
 
-def get_print_bucket_contents_actions():
-    return Actions(
+def get_print_bucket_contents_actions() -> TestActions:
+    return TestActions(
         pre_test=lambda mock_bucket: print_objects_in_bucket("existed before running the test.", mock_bucket),
         during_test=lambda mock_bucket: print_objects_in_bucket("existed during the test.", mock_bucket),
         post_test=lambda mock_bucket: print_objects_in_bucket("exist after the test.", mock_bucket),
