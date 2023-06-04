19493a6be304ef03b46f1fcc72d4f5e7a7b29f43
Warren <rabbitsinwarrens@gmail.com>
2023-06-04 22:34:50 +0000

    Add 'env' variable to wrapped_test method.

--- tests/utilities/infrastructure.py
+++ tests/utilities/infrastructure.py
@@ -1,26 +1,33 @@
 import colorama
 import os
+import pytest
 import signal
 
 from colorama import Fore, Style
-from etl.mock.infrastructure import s3_bucket
-from etl.mock.infrastructure import buckets as test_infrastructure
+from etl.mock.infrastructure import mock_s3_server
+from etl.mock.infrastructure.s3_resource import S3ResourceSingleton
+import etl.mock.infrastructure.buckets as buckets
 from etl.paths.components import Bucket
 from tests.utilities.actions import TestActions
+from etl.paths.components import Bucket
+
+ENV = Bucket.MOCK
+
 
 def with_test_server(actions:TestActions):
     def inner_decorator(test_function):
-        def wrapped_test(glueContext):
+        def wrapped_test(glueContext, s3_resource):
             spark = glueContext.spark_session
-            mock_bucket, process = s3_bucket.get_mock_s3_server_and_its_local_process(
-                                                spark, 
-                                                name=Bucket.PROJECT.value,
+            S3ResourceSingleton.teardown()
+            mock_bucket, process = mock_s3_server.get_mock_s3_server_and_its_local_process(
+                                                spark,
+                                                name=str(ENV),
                                                 endpoint_url="http://127.0.0.1:5000/")   
-            test_infrastructure.initialise_for_project(mock_bucket)
+            buckets.initialise_for_project(mock_bucket)
 
             try:
                 actions.pre_test(mock_bucket)
-                test_function(spark)
+                test_function(spark, env=ENV)
                 actions.during_test(mock_bucket)   
             finally:
                 mock_bucket.unload_all()
@@ -31,3 +38,5 @@ def with_test_server(actions:TestActions):
     return inner_decorator
 
 
+
+    ...
