9f9df251201f73b6987e8d1990d7972691ffa668
Warren <rabbitsinwarrens@gmail.com>
2023-06-04 22:38:23 +0000

    Add s3 resource teardowns.

---
+++ tests/mock_ojects/test_mock_landing_data.py
@@ -0,0 +1,38 @@
+import os
+import signal
+import subprocess
+from pyspark.sql import SparkSession
+from etl.mock.data import claim, policyholder, provider
+from etl.mock.infrastructure.buckets import initialise_for_project
+import etl.mock.infrastructure.mock_s3_server as s3_mock_server
+from etl.paths.components import Bucket
+
+ENV = Bucket.TEST
+s3_mock_server.S3ResourceSingleton.teardown()
+
+def test_row_count_for_all_source_tables_in_landing_tier(glueContext):
+    s3_mock_server.S3ResourceSingleton.teardown()
+    spark = glueContext.spark_session    
+    mock_bucket, process = s3_mock_server.get_mock_s3_server_and_its_local_process(
+                                        spark, 
+                                        name=str(ENV),
+                                        endpoint_url="http://127.0.0.1:5000/")
+
+    try:   
+        initialise_for_project(mock_bucket)
+        source = mock_bucket.get_source_facts()
+        
+        for obj in mock_bucket.list_all():
+            df = spark.read.csv(f"s3://{obj.bucket_name}/{obj.key}", header=True)
+            df.show(5, truncate=True)
+            if 'claim-' in obj.key:
+                assert df.count() == source["claim"]["count"]
+            if 'provider-' in obj.key:
+                assert df.count() == source["provider"]["count"]
+            if 'policyholder-' in obj.key:
+                assert df.count() == source["policyholder"]["count"]
+        
+    finally:
+        mock_bucket.unload_all()
+        s3_mock_server.S3ResourceSingleton.teardown()
+        os.killpg(os.getpgid(process.pid), signal.SIGTERM)
---
+++ tests/mock_ojects/test_mock_s3.py
@@ -0,0 +1,44 @@
+import os
+import boto3
+import signal
+import subprocess
+import pytest
+from etl.jobs.landing.claim import read_data, transform_data, write_data
+from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+from etl.mock.infrastructure.mock_s3_server import S3ResourceSingleton, \
+    get_mock_s3_server_and_its_local_process
+from etl.paths.components import Bucket
+
+SOURCE_NAME = "data.csv"
+TABLE_NAME = "dummy"
+BUCKET = Bucket.MOCK
+ENDPOINT_URL = "http://127.0.0.1:5000/"
+S3ResourceSingleton.teardown()
+
+
+@pytest.mark.slow
+def test_server():
+    if S3ResourceSingleton._instance != None:
+        S3ResourceSingleton.teardown() 
+    
+    spark = SparkSession.builder.appName("test").getOrCreate()
+    mock_bucket, process = get_mock_s3_server_and_its_local_process(
+                            spark,
+                            name = str(BUCKET),
+                            endpoint_url = ENDPOINT_URL)
+    print(f"bucket: {mock_bucket.bucket.name}")
+    data = 'name,dt\nsam,1962-05-25\nlet,1999-05-21\nnick,1996-04-03\nperson,1996-04-03\npersonb,1996-03-02'
+    mock_bucket.load(Key="data.csv", Body=data)
+    try: 
+        df = spark.read.csv(f"s3://{str(BUCKET)}/data.csv", header=True)
+        df.show()
+        assert df.count() == 5
+        assert df.columns == ['name', 'dt']
+        spark.stop()
+    finally:
+        print("Finally....")
+        if S3ResourceSingleton._instance != None:
+             print("{S3ResourceSingleton._instance} exists. Deleting...")
+             S3ResourceSingleton.teardown()
+        os.killpg(os.getpgid(process.pid), signal.SIGTERM)
