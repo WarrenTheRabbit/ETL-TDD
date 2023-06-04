e6cf084c5d1c58614ff1f20348ad2dd6e512ca2b
Warren <rabbitsinwarrens@gmail.com>
2023-06-04 22:42:30 +0000

    Update schemas for compatability with DataBrew.

--- etl/validation/schemas/landing.py
+++ etl/validation/schemas/landing.py
@@ -1,17 +1,17 @@
-from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DateType, DecimalType
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, FloatType, DateType, DecimalType
 
 CLAIM:StructType = StructType(
         [
             StructField("claim_id", IntegerType(), True),
             StructField("policy_holder_id", IntegerType(), True),
             StructField("provider_id", IntegerType(), True),
-            StructField("date_of_service", DateType(), True),
+            StructField("date_of_service", StringType(), True),
             StructField("procedure", StringType(), True),
-            StructField("total_procedure_cost", DecimalType(10, 2), True),
-            StructField("medibank_pays", DecimalType(10, 2), True),
-            StructField("medicare_pays", DecimalType(10, 2), True),
-            StructField("excess", DecimalType(10, 2), True),
-            StructField("out_of_pocket", DecimalType(10, 2), True)
+            StructField("total_procedure_cost", FloatType(), True),
+            StructField("medibank_pays", FloatType(), True),
+            StructField("medicare_pays", FloatType(), True),
+            StructField("excess", FloatType(), True),
+            StructField("out_of_pocket", FloatType(), True)
         ])
 
 POLICYHOLDER:StructType = StructType(
@@ -19,14 +19,14 @@ POLICYHOLDER:StructType = StructType(
         StructField("policy_holder_id", IntegerType(), True),
         StructField("first_name", StringType(), True),
         StructField("last_name", StringType(), True),
-        StructField("date_of_birth", DateType(), True),
+        StructField("date_of_birth", StringType(), True),
         StructField("gender", StringType(), True),
         StructField("address", StringType(), True),
         StructField("phone_number", StringType(), True),
         StructField("email_address", StringType(), True),
         StructField("insurance_plan_details", StringType(), True),
-        StructField("coverage_start_date", DateType(), True),
-        StructField("coverage_end_date", DateType(), True),
+        StructField("coverage_start_date", StringType(), True),
+        StructField("coverage_end_date", StringType(), True),
         StructField("policy_standing", StringType(), True)
     ])
 
