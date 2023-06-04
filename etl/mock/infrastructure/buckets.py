74862a08e45c89bb7300ec3346134d7b55f7edd8
Warren <rabbitsinwarrens@gmail.com>
2023-06-04 22:41:25 +0000

    Rename and update imports.

--- etl/mock/infrastructure/buckets.py
+++ etl/mock/infrastructure/buckets.py
@@ -1,6 +1,7 @@
-from etl.mock.infrastructure.s3_bucket import MockS3Bucket
+from etl.mock.infrastructure.mock_s3_bucket import MockS3Bucket
 
 def initialise_for_project(mock_bucket: MockS3Bucket):
+    print(mock_bucket.bucket)
     mock_bucket.unload_all()
     if mock_bucket.list_all() != []:
         raise Exception("Mock bucket is NOT empty after `unload_all`.")
