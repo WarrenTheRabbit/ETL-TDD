96b89cf3ae7b382375a7f99a6c2b301b7ec74825
Warren <rabbitsinwarrens@gmail.com>
2023-06-05 11:35:42 +0000

    docs(comment): correct punctuation

--- etl/mock/infrastructure/s3_resource.py
+++ etl/mock/infrastructure/s3_resource.py
@@ -7,7 +7,7 @@ class S3ResourceSingleton:
         """
         TODO: Add docstring.
         """
-        # Guarantee that `region_name` is set; default to 'us-east-1'.
+        # Guarantee that `region_name` is set by defaulting to 'us-east-1'.
         region_name = kwargs.get('region_name','us-east-1') 
         kwargs['region_name'] = region_name 
         print(f"kwargs is {kwargs}")
@@ -21,23 +21,19 @@ class S3ResourceSingleton:
                                     **kwargs)
         else:
             S3ResourceSingleton._instance = boto3.resource("s3",
-                                                  region_name="us-east-1")
+                                                  region_name=region_name)
         self = S3ResourceSingleton._instance
 
     @staticmethod
     def getInstance(**kwargs):
-        print(f"getInstance(**kwargs={kwargs}) called")
         if S3ResourceSingleton._instance is None:
             S3ResourceSingleton(is_mock=False, **kwargs)
-        print(f"endpoint is {S3ResourceSingleton._instance.meta.client._endpoint}")
         return S3ResourceSingleton._instance
 
     @staticmethod
     def getMockInstance(**kwargs):
-        print(f"getMockInstance(**kwargs={kwargs}) called")
         if S3ResourceSingleton._instance is None:
             S3ResourceSingleton(is_mock=True, **kwargs)
-        print(f"endpoint is {S3ResourceSingleton._instance.meta.client._endpoint}")
         return S3ResourceSingleton._instance
     
     @staticmethod
