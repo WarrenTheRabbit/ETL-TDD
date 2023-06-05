960f6db8fb6521c1382fb053cde41040673a13b3
Warren <rabbitsinwarrens@gmail.com>
2023-06-05 08:24:38 +0000

    Use `spark` instead of `glueContext`.

--- stage_claim_into_raw.py
+++ stage_claim_into_raw.py
@@ -13,28 +13,27 @@ from etl.validation.schemas import landing as claim_schema
 from etl.jobs.landing.claim import read_data, transform_data, write_data, \
     get_input_path, get_output_path
 
-def run(glueContext:GlueContext,env):
+def run(spark:SparkSession,env):
     
     # Read in data needed for transformations.    
     read_path = get_input_path(env)
-    read_df:DataFrame = read_data(engine=glueContext.spark_session, 
+    read_df:DataFrame = read_data(engine=spark, 
                                   path=read_path, 
                                   schema=claim_schema.CLAIM,
                                   header=True)
 
     # Apply transformations.  
     transformed_df:DataFrame = transform_data(read_df)
-    dynamic_frame = DynamicFrame.fromDF(transformed_df, glueContext, "transformed_dyf")
     
     # Write dynamic frame to path.
     write_path = get_output_path(env)
-    write_data(df=dynamic_frame, 
+    write_data(df=transformed_df, 
                path=write_path, 
                mode='overwrite') 
     
     # Return transformed data for optional testing, and write_path for 
     # basing automation tasks on the s3 path.  
-    return dynamic_frame, write_path
+    return transformed_df, write_path
     
 if __name__ == '__main__':
     args = getResolvedOptions(sys.argv, ['JOB_NAME'])
@@ -42,5 +41,6 @@ if __name__ == '__main__':
     glueContext = GlueContext(sc)
     job = Job(glueContext)
     job.init(args['JOB_NAME'], args)
-    run(glueContext,env=Bucket.PROD)
+    spark = glueContext.spark_session
+    run(spark, env=Bucket.PROD)
     job.commit()
