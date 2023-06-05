d2dd9e6e14b0c7eac17ae3a78a1777f3d9ebbd52
Warren <rabbitsinwarrens@gmail.com>
2023-06-05 09:44:09 +0000

    Cast SCDs to strings for DataBrew compatability.

--- etl/jobs/optimised/attributes.py
+++ etl/jobs/optimised/attributes.py
@@ -5,9 +5,9 @@ from pyspark.sql.functions import current_timestamp, from_unixtime, lit, monoton
 def with_slowly_changing_dimensions(df:DataFrame) -> DataFrame:
     return (df
         .withColumn("track_hash", lit(None).cast("bigint"))
-        .withColumn("record_start_ts", lit("1970-01-01 00:00:00").cast("timestamp")) 
-        .withColumn("record_end_ts", lit("2999-12-31 00:00:00").cast("timestamp")) 
+        .withColumn("record_start_ts", lit("1970-01-01 00:00:00").cast("string")) 
+        .withColumn("record_end_ts", lit("2999-12-31 00:00:00").cast("string")) 
         .withColumn("record_active_flag", lit(1).cast("smallint"))
-        .withColumn("record_upd_ts", current_timestamp())
-        .withColumn("record_insert_ts", current_timestamp())
+        .withColumn("record_upd_ts", current_timestamp().cast("string"))
+        .withColumn("record_insert_ts", current_timestamp().cast("string"))
     )
