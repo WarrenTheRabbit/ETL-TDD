07678154ff901625ea08473407d3edcea7da8c7f
Warren <rabbitsinwarrens@gmail.com>
2023-06-07 02:18:26 +0000

    feat(batches): start organising as a package

---
+++ etl/batches/batch1.py
@@ -0,0 +1,8 @@
+def get_batch(**config):
+    
+   return Batch([
+                  stage_claim_into_raw, 
+                  stage_policyholder_into_raw, 
+                  stage_provider_into_raw
+               ], 
+               **config)
