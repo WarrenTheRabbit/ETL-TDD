f1737056e4ccc8c38314bcea257941bad3410770
Warren <rabbitsinwarrens@gmail.com>
2023-06-07 11:27:23 +0000

    chore(batches): add shell script for manual start

--- automation/shell_utilities/simple_end_to_end.sh
+++ automation/shell_utilities/simple_end_to_end.sh
@@ -1,25 +1,7 @@
 #!/bin/bash
 
-# Depends on claim source data
-python3 claim_landing.py --JOB_NAME test && python3 claim_raw.py --JOB_NAME test
-
-# Depends on policyholder source data
-python3 policyholder_landing.py --JOB_NAME test && python3 policyholder_raw.py --JOB_NAME test
-
-# Depends on provider source data
-python3 provider_landing.py --JOB_NAME test && python3 provider_raw.py --JOB_NAME test
-
-# No dependencies
-python3 date_dimension_optimsied.py --JOB_NAME test
-
-# Depends on policyholder_raw.py and provider_raw.py
-python3 location_dimension_optimised.py --JOB_NAME test
-
-# Depends on location_dimension_optimised.py
-python3 provider_dim_optimised.py --JOB_NAME test
-
-# Depends on location_dimension_optimised.py
-python3 policyholder_dim_optimised.py --JOB_NAME test
-
-# Depends on policyholder_dim_optimised.py and provider_dim_optimised.py
-python3 claim_access.py --JOB_NAME test
+python3 batch1.py --JOB_NAME anything && 
+python3 batch2.py --JOB_NAME anything &&
+python3 batch3.py --JOB_NAME anything &&
+python3 batch4.py --JOB_NAME anything &&
+python3 batch5.py --JOB_NAME anything
